59f609541a27b791ea5cf02a50396b3a4befe44d
unknown <yassboudra@gmail.com>
2020-07-25 22:12:43 +0200

    first commit

--- client.js
+++ client.js
@@ -1,7 +1,8 @@
 const io = require('socket.io-client');
-
+console.log("DDD");
 const joinSession = () =>{
     const port = process.env.PORT || 4000;
     const socket = io.connect(`http://localhost:${port}`);
     socket.emit('join', 'dfdsfsd');
+
 }
