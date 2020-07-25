db0d6051332ab0acae3f1ce378c8c7de8eef44e4
unknown <yassboudra@gmail.com>
2020-07-25 20:55:19 +0200

    first commit

---
+++ client.js
@@ -0,0 +1,8 @@
+const joinSession = () =>{
+    const io = require('socket.io-client');
+    const port = process.env.PORT || 4000;
+    const socket = io.connect(`http://localhost:${port}`);
+    socket.emit('join', 'dfdsfsd');
+}
+
+joinSession();
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const chat = express();
+
+const http = require("http");
+const server = http.createServer(chat);
+
+const io = require('socket.io')(server)
+
+io.on('connection', (socket) => {
+    socket.on('join', (data) => {
+        console.log(data);
+    })
+});
+
+chat.use(express.static(__dirname + '/'));
+
+chat.get('/', (req, res) => {
+    res.sendFile('/', {
+        root: __dirname
+    });
+});
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+});
