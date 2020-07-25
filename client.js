fb2044fa064fe77940f857a25b60c0c87a432b6c
unknown <yassboudra@gmail.com>
2020-07-25 22:42:20 +0200

    first commit

--- client.js
+++ client.js
@@ -1,10 +1,28 @@
 const io = require('socket.io-client');
 
-const joinSession = () =>{
-    const socket = io.connect(`https://obitoschat.herokuapp.com/`);
-    const alias = document.getElementById('alias').value
-    socket.emit('join', alias);
-    socket.on('join', (data) => {
-        document.getElementById('screen').innerHTML = data;
-    });
+const chatContainer = document.getElementById("chat-container");
+const alias = document.getElementById("alias");
+const label = document.getElementById("label");
+const login = document.getElementById("login");
+const chat = document.getElementById("chat");
+const screen = document.getElementById("screen");
+
+chatContainer.style.height = window.innerHeight + 'px';
+
+function joinChat() {
+    if(alias.value === '') {
+        label.innerHTML = '<b>Please create an alias!!!</b>';
+    } else {
+        login.style.display = 'none';
+        chat.style.display = 'block';
+        
+        const joinSession = () =>{
+            const socket = io.connect(`https://obitoschat.herokuapp.com/`);
+            const alias = document.getElementById('alias').value
+            socket.emit('join', alias);
+            socket.on('join', (data) => {
+                document.getElementById('screen').innerHTML = data;
+            });
+        }
+    }
 }
