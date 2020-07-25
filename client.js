47865cece3f384fd4d34c01b657c26e6dbed77aa
unknown <yassboudra@gmail.com>
2020-07-25 21:53:51 +0200

    first commit

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
-const joinSession = () =>{
+const io = require('socket.io-client');
 
+const joinSession = () =>{
+    const port = process.env.PORT || 4000;
     const socket = io.connect(`http://localhost:${port}`);
     socket.emit('join', 'dfdsfsd');
 }
--- main.js
+++
@@ -1,6 +0,0 @@
-const uniq = require('uniq');
-const io = require('socket.io');
-
-const data = [1];
-
-console.log(uniq(data));
