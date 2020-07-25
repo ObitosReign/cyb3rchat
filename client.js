fc17ba2a615060efb7cafb592e55cf55c29f5529
unknown <yassboudra@gmail.com>
2020-07-25 22:20:59 +0200

    first commit

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const io = require('socket.io-client');
-console.log("DDD");
+
 const joinSession = () =>{
     const socket = io.connect(`https://obitoschat.herokuapp.com/`);
     socket.emit('join', 'dfdsfsd');
-
+    console.log("DDD");
 }
