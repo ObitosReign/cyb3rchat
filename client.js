a4055549106e643105600d1ca931c69e7de148bd
yassine <yassboudra@gmail.com>
2022-04-04 18:50:50 +0200

    make it better

--- client.js
+++ client.js
@@ -12,7 +12,7 @@ chatContainer.style.height = window.innerHeight + 'px';
 
 const port = 9002;
 
-const socket = io.connect(`https://0.0.0.0:${port}`);
+const socket = io.connect(`https://cyb3rchat.herokuapp.com:${port}`);
 
 function joinSession() {
     if(alias.value === '') {
