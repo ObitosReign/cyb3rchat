fa22e0a6268eff4980da52100cd11a374e8e0162
yassine <yassboudra@gmail.com>
2022-04-11 18:33:20 +0200

    made it better

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const io = require('socket.io-client');
 const chatContainer = document.getElementById("chat-container");
-const alias = document.getElementById("alias");
+const alias = document.getElementById("input-alias");
 const label = document.getElementById("label");
 const login = document.getElementById("login");
 const chat = document.getElementById("chat");
